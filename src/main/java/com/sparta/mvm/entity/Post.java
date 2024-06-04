ef77ae7fe5694886480fba8fc3bbe4cdab59f4a5
LeeNaYoung240 <ajd0927@naver.com>
2024-06-04 20:37:27 +0900

    Add : 게시글, 시간 클래스 생성
    Post, Timestamped Entity 생성

---
+++ src/main/java/com/sparta/mvm/entity/Post.java
@@ -0,0 +1,28 @@
+package com.sparta.mvm.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.catalina.User;
+
+@Entity
+@Getter
+@Table(name = "post")
+@NoArgsConstructor
+public class Post extends Timestamped{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "contents",nullable = false)
+    private String contents;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    public Post(String contents, User user) {
+        this.contents = contents;
+        this.user = user;
+    }
+}
