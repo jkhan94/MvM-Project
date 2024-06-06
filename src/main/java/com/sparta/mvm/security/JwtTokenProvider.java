e17d02077f0ad294a6cbd2a3dde1213829494bc7
gminnimk <tvt1245@naver.com>
2024-06-07 08:41:45 +0900

    Add : JWT 토큰을 생성 & 검증 하는 JwtTokenProvider 클래스 생성

---
+++ src/main/java/com/sparta/mvm/security/JwtTokenProvider.java
@@ -0,0 +1,15 @@
+package com.sparta.mvm.security;
+
+
+// JWT 토큰을 생성하고 검증하는 클래스
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+
+}
