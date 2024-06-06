961eeaae988e0adbe1e49df253658166a946d14e
jkhan94 <hjkwork@naver.com>
2024-06-06 23:29:52 +0900

    Refactor : PasswordConfig
    주석 제거

--- src/main/java/com/sparta/mvm/config/PasswordConfig.java
+++ src/main/java/com/sparta/mvm/config/PasswordConfig.java
@@ -1,5 +1,4 @@
 package com.sparta.mvm.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -7,10 +6,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class PasswordConfig {
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        // interface PasswordEncoder 구현체
-        // BCrypt는 해시함수. 강력해서 비밀번호 암호화(패스워드 인코딩)에 많이 쓰임.
         return new BCryptPasswordEncoder();
     }
 }
