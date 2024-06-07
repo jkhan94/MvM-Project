9cc35f654b69e180b16855e63540423735897b87
gminnimk <tvt1245@naver.com>
2024-06-07 15:19:22 +0900

    Fix : Optional<TestUser>을 반환하도록 수정

--- src/main/java/com/sparta/mvm/AuthTest/TestUserRepository.java
+++ src/main/java/com/sparta/mvm/AuthTest/TestUserRepository.java
@@ -2,6 +2,12 @@ package com.sparta.mvm.AuthTest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+//public interface TestUserRepository extends JpaRepository<TestUser, Long> {
+//    TestUser findByUsername(String username);
+//}
+
 public interface TestUserRepository extends JpaRepository<TestUser, Long> {
-    TestUser findByUsername(String username);
-}
+    Optional<TestUser> findByUsername(String username);
+} 
