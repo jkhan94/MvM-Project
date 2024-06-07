61acd8136692cff5570f6ec3c5619ca3af4bdbc5
gminnimk <tvt1245@naver.com>
2024-06-07 14:44:45 +0900

    Fix : 클래스로 임시 주석 처리

--- src/main/java/com/sparta/mvm/controller/AuthController.java
+++ src/main/java/com/sparta/mvm/controller/AuthController.java
@@ -1,28 +1,28 @@
-package com.sparta.mvm.controller;
-
-
-
-// 로그인 요청을 처리하는 컨트롤러 클래스
-
-
-import com.sparta.mvm.dto.LoginRequestDto;
-import com.sparta.mvm.service.AuthService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("api/auth")
-public class AuthController {
-    private final AuthService authService;
-
-    @PostMapping("/login")
-    public ResposeEntity<TokenResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
-        TokenResponseDto tokenResponseDto = authService.login(loginRequestDto);
-        return ResponseEntity.ok(tokenRequestDto);
-    }
-}
+//package com.sparta.mvm.controller;
+//
+//
+//
+//// 로그인 요청을 처리하는 컨트롤러 클래스
+//
+//
+//import com.sparta.mvm.dto.LoginRequestDto;
+//import com.sparta.mvm.service.AuthService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("api/auth")
+//public class AuthController {
+//    private final AuthService authService;
+//
+//    @PostMapping("/login")
+//    public ResposeEntity<TokenResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+//        TokenResponseDto tokenResponseDto = authService.login(loginRequestDto);
+//        return ResponseEntity.ok(tokenRequestDto);
+//    }
+//}
