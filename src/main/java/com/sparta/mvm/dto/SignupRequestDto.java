751df6ae48f245fe35b8183c1607e9ac8a8d3973
leesewon <leesw1945@gmail.com>
2024-06-10 11:26:39 +0900

    Refactor : SignupRequestDto 수정

--- src/main/java/com/sparta/mvm/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/mvm/dto/SignupRequestDto.java
@@ -10,19 +10,19 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SignupRequestDto {
-    @NotBlank
+    @NotBlank(message = "ID는 공백일 수 없습니다.")
     @Size(min = 10, max = 20, message = "아이디는 최소 10자 이상, 20자 이하로 작성해주세요.")
     @Pattern(regexp = "^[A-Za-z0-9]+$", message = "아이디는 대소문자 포함 영문 + 숫자만을 허용합니다.")
     private String username;
-    @NotBlank
+    @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
     @Size(min = 10, message = "비밀번호는 최소 10자 이상 작성해주세요.")
     @Pattern(regexp = "^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{10,}$",
             message = "대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 포함해야합니다. ")
     private String password;
-    @NotBlank
+    @NotBlank(message = "이름은 공백일 수 없습니다.")
     private String name;
     private String lineIntro;
     @Email
-    @NotBlank
+    @NotBlank(message = "email은 공백일 수 없습니다.")
     private String email;
 }
