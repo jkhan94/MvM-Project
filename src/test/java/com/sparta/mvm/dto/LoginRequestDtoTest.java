e864737b22ddabb6f7489cf86a3ce927a91a9785
jkhan94 <hjkwork@naver.com>
2024-06-14 16:59:54 +0900

    test(DTO): LoginRequestDtoTest setter 확인 테스트 추가

---
+++ src/test/java/com/sparta/mvm/dto/LoginRequestDtoTest.java
@@ -0,0 +1,30 @@
+package com.sparta.mvm.dto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginRequestDtoTest {
+    LoginRequestDto requestDto;
+
+    @Test
+    @DisplayName("Setter 동작 확인")
+    void testSetter() {
+        // given
+        String username = "Hello";
+        String password = "World#11111";
+
+        // when
+        requestDto = new LoginRequestDto();
+        requestDto.setUsername(username);
+        requestDto.setPassword(password);
+
+        // then
+        assertNotNull(requestDto);
+        assertEquals(username, requestDto.getUsername());
+        assertEquals(password, requestDto.getPassword());
+    }
+
+
+}
