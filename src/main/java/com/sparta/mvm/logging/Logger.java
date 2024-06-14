816cf45d6803ddfc1ca93ec58926dfb5754cd3b2
jkhan94 <hjkwork@naver.com>
2024-06-14 12:49:00 +0900

    feat : AOP 의존성 추가, 로깅 기능 추가

---
+++ src/main/java/com/sparta/mvm/logging/Logger.java
@@ -0,0 +1,28 @@
+package com.sparta.mvm.logging;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+// 모든 API(Controller)가 호출될 때,
+// Request 정보(Request URL, HTTP Method)를 Log로 출력
+@Component
+@Aspect
+@Slf4j
+public class Logger {
+    @Pointcut("execution(* com.sparta.mvm.controller.*.*(..))")
+    private void allController() { }
+
+    @Before("allController()")
+    public void beforeRequestInfo(JoinPoint joinPoint){
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        log.info(request.getMethod() + " : "+request.getRequestURI());
+    }
+
+}
